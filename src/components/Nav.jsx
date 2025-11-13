9e9ce3bc969e6e9d7eefd42232bfb6bcbba1f71b
Ritzs <ritanshubabuta399@gmail.com>
2025-11-14 00:32:50 +0530

    Task completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,6 @@ export default function Nav() {
   return (
     <nav className="w-full p-4 bg-[linear-gradient(90deg,#0b2545,#08203a)] text-white shadow-md">
       <div className="container mx-auto flex items-center justify-between">
-        <img className='w-[5%] rounded-[100%] h-[5%]' src="..\src\Assets\images\logo.jpg" alt="" />
         <Link to="/" className="xl:text-5xl .textsize sm:text-3xl font-semibold">User Manager</Link>
         <div className="space-x-2">
           <Link to="/" className="px-3 py-1 rounded hover:bg-white/10">Home</Link>
